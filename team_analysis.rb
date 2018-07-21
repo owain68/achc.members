f71897c9f8133d077926b77fab1ae45645838f88
Owain McGuire <owain@alpenta.com>
2018-07-21 12:12:02 +0100

    shows strange or lack of DOB by team

--- team_analysis.rb
+++ team_analysis.rb
@@ -8,6 +8,7 @@ analysis_date = ARGV[1] ? Date.parse(ARGV[1]) : Date.today
 team_collection = {}
 record_count = 0;
 member_stats = 0;
+TEAM_REGEX = /U(\d+)/
 
 CSV.foreach(INPUT_CSV_FILE, headers: true) do |row|
   record_count += 1
@@ -20,18 +21,23 @@ CSV.foreach(INPUT_CSV_FILE, headers: true) do |row|
     else
       team_collection[team] = {}
       team_collection[team][:count] = 1
+      team_collection[team][:age_group] = team.scan(TEAM_REGEX).last.first if team =~ TEAM_REGEX
+      team_collection[team][:members] = []
     end
     if team_collection[team][row['gender']]
       team_collection[team][row['gender']] += 1
     else
       team_collection[team][row['gender']] = 1
     end
-    year_group = YearGroup.on Date.today, row['dateofbirth']
+    year_group = YearGroup.on analysis_date, row['dateofbirth']
     if team_collection[team][year_group]
       team_collection[team][year_group] += 1
     else
       team_collection[team][year_group] = 1
     end
+    entry = {firstname: row['FirstName'], lastname: row['Surname'], dob: row['dateofbirth'], year: year_group}
+    entry[:age] = YearGroup.age_on(analysis_date, Date.parse(row['dateofbirth'])) if row['dateofbirth']
+    team_collection[team][:members] << entry
   end
 end
 
@@ -43,9 +49,18 @@ puts "#{team_collection.size} teams found"
 
 team_collection.sort.to_h.each do |team, stats|
   next unless team =~ /Junior|Academy|U\d/
+  puts
   print team
   stats.each do |k, v|
+    next if k == :members
     print ", #{k}->#{v}"
   end
+  next unless stats[:age_group]
   print "\n"
+  puts "\n Investigate"
+  normal_ages = [stats[:age_group].to_i - 5, stats[:age_group].to_i - 6]
+  stats[:members].each do |m|
+    highlight = normal_ages.include?(m[:age]) ? '' : '<<<<<<<<'
+    printf("  %-40s %-12s  %-7s %-2s     %-10s\n", m[:firstname] + ' ' + m[:lastname], m[:dob], m[:year], m[:age], highlight)
+  end
 end
